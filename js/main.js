246149f563354f5ad38bd5d0870c4d3da21508b2
green2356 <87744000+green2356@users.noreply.github.com>
2021-08-03 14:55:30 +0900

    top 버튼 추가, 하단 고정메뉴 삭제

--- js/main.js
+++ js/main.js
@@ -93,4 +93,17 @@ $(function(){
         }); 
     });
     
-});
+    // top
+
+    // 보이기 | 숨기기 
+    $(window).scroll(function() { if ($(this).scrollTop() > 250) {
+        // 스크롤 250 넘으면 버튼이 보여지게
+        $('.top').fadeIn(); } else { $('.top').fadeOut(); } });
+        // 버튼 클릭시
+        $(".top").click(function() {
+            $('html, body').animate({
+                scrollTop : 0 // 0 까지 animation 이동합니다.
+            }, 400);        // 속도 400
+            return false; 
+        });
+    });
